603d45cf980fe297a509415a61bd683c39a056eb
Kosta <konstantin.toup9@gmail.com>
2026-07-03 21:12:25 +0900

    add units & some exception logic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import httpx
 from pydantic import BaseModel, Field
 
@@ -22,27 +22,32 @@ async def health():
 async def direct_geocoding(city_name: str, limit: int=1) -> tuple:
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={limit}&appid={owm_api}"
 
+
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         response = response.json()
 
+    if len(response) == 0: return -1, -1
     result = (response[0].get("lat"), response[0].get("lon"))
     return result
 
-@app.get("/{city_name}")
+@app.get("/weather/{city_name}")
 async def get_weather(city_name: str):
     lat, lon = await direct_geocoding(city_name)
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={owm_api}"
+    if (lat, lon) == (-1, -1): raise HTTPException(status_code=404, detail="Вы ввели несуществующий город, повторите попытку")
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={owm_api}&lang=ru"
 
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         response = response.json()
+
 
     result = WeatherResponse(
         city=city_name,
         weather=response.get("weather")[0].get("main"),
-        temperature=round(float(response.get("main").get("temp")) - 273.15, 2),
-        feels_temperature=round(float(response.get("main").get("feels_like")) - 273.15, 2)
+        temperature=response.get("main").get("temp"),
+        feels_temperature=response.get("main").get("feels_like")
     )
 
     return result
