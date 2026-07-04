c72478374cc7ef212c656c00ad0e45335d8acd7b
Kosta <konstantin.toup9@gmail.com>
2026-07-04 19:38:52 +0900

    feat: add weather router to main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from app.clients.weather_client import get_weather
 app = FastAPI()
+
+weather_router = APIRouter(prefix="/weather", tags=["weather"])
 
 @app.get("/health")
 async def health():
     return {"status": "OK"}
 
-@app.get("/weather/{city_name}")
+@weather_router.get("/{city_name}")
 async def get_weather_owm(city_name: str):
     return await get_weather(city_name)
+app.include_router(weather_router)
